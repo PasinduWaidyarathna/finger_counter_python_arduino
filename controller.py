1d8005392dff778fc81a09eb3d48626dbb734bef
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2023-06-29 17:04:02 +0530

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,64 @@
+import pyfirmata
+
+comport = 'COM3' #port number (USB Plugged)
+
+board = pyfirmata.Arduino(comport)
+
+led_1 = board.get_pin('d:2:o')
+led_2 = board.get_pin('d:3:o')
+led_3 = board.get_pin('d:4:o')
+led_4 = board.get_pin('d:5:o')
+led_5 = board.get_pin('d:6:o')
+led_6 = board.get_pin('d:7:o')
+led_7 = board.get_pin('d:8:o')
+
+
+def led(total):
+    if total == 0:
+        led_1.write(0)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(0)
+        led_6.write(0)
+        led_7.write(1)
+    elif total == 1:
+        led_1.write(1)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(1)
+        led_5.write(1)
+        led_6.write(1)
+        led_7.write(1)
+    elif total == 2:
+        led_1.write(0)
+        led_2.write(0)
+        led_3.write(1)
+        led_4.write(0)
+        led_5.write(0)
+        led_6.write(1)
+        led_7.write(0)
+    elif total == 3:
+        led_1.write(0)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(1)
+        led_6.write(1)
+        led_7.write(0)
+    elif total == 4:
+        led_1.write(1)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(1)
+        led_5.write(1)
+        led_6.write(0)
+        led_7.write(0)
+    elif total == 5:
+        led_1.write(0)
+        led_2.write(1)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(1)
+        led_6.write(0)
+        led_7.write(0)
